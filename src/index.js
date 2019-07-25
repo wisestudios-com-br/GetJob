c0b7d0d361b470467d9895aaf0d7358986aa80d9
JGabriel Gruber <jgabrielgruber@hotmail.com>
2019-07-25 16:10:28 -0300

    Implementado router;

--- src/index.js
+++ src/index.js
@@ -1,16 +1,14 @@
 import React from 'react'
 import { render } from 'react-dom'
-import { Provider } from 'react-redux'
-import App from './components/App'
+
 import * as serviceWorker from './serviceWorker';
 import configureStore from './configureStore'
+import Root from './components/Root'
 
 const store = configureStore()
 
 render(
-	<Provider store={store}>
-		<App />
-	</Provider>,
+	<Root store={store} />,
 	document.getElementById('root')
 )
 // If you want your app to work offline and load faster, you can change
